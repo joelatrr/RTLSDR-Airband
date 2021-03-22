4172a0d5854a12c8be2aef26c4f0f8a6a6328602
Joel Palmtag <67025384+joelatrr@users.noreply.github.com>
2021-03-22 09:58:37 -0700

    Testing Github comment limits

--- input-rtlsdr.h
+++ input-rtlsdr.h
@@ -17,6 +17,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+// adding a line here so I can post a merge
 #include <rtl-sdr.h>		// rtlsdr_dev_t
 #define RTLSDR_BUFSIZE 320000
 #define RTLSDR_DEFAULT_LIBUSB_BUFFER_COUNT 10
